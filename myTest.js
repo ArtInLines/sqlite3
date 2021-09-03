a178d3836dc62198ab88c50427e4b73886748272
ArtInLines <linesinart@gmail.com>
2021-09-03 19:25:46 +0200

    Tested & Bugfixed createJoinSubquery method

--- myTest.js
+++ myTest.js
@@ -2,7 +2,7 @@ const SQLiteDB = require('./lib/SQLiteDB');
 
 let db = new SQLiteDB('temp.db');
 
-console.log(db.db.prepare('CREATE TABLE IF NOT EXISTS cats ( name TEXT, age INTEGER, color TEXT )').run());
-console.log(db.db.prepare('CREATE TABLE IF NOT EXISTS people ( name TEXT, age INTEGER )').run());
-// console.log(db.insert('cats', ['name', 'age'], ['Sissy', 1]));
-console.log(db.tables());
+// console.log(db.db.prepare('CREATE TABLE IF NOT EXISTS cats ( name TEXT, age INTEGER, color TEXT )').run());
+// console.log(db.db.prepare('CREATE TABLE IF NOT EXISTS people ( name TEXT, age INTEGER )').run());
+// console.log(db.insert('people', ['name', 'age'], ['Baby', 1]));
+// console.log(db.tables());
