ec139910fab23da022d5e27ad3296f6330dd8c6a
ArtInLines <linesinart@gmail.com>
2021-09-04 02:08:45 +0200

    Added tested deleteTable method

--- myTest.js
+++ myTest.js
@@ -1,8 +1,15 @@
 const SQLiteDB = require('./lib/SQLiteDB');
 
-let db = new SQLiteDB('temp.db');
+let db = new SQLiteDB('temp.db', { verbose: console.log });
 
-// console.log(db.db.prepare('CREATE TABLE IF NOT EXISTS cats ( name TEXT, age INTEGER, color TEXT )').run());
-// console.log(db.db.prepare('CREATE TABLE IF NOT EXISTS people ( name TEXT, age INTEGER )').run());
-// console.log(db.insert('people', ['name', 'age'], ['Baby', 1]));
-// console.log(db.tables());
+db.db.prepare('CREATE TABLE IF NOT EXISTS cats ( name TEXT, age INTEGER, color TEXT )').run();
+db.db.prepare('CREATE TABLE IF NOT EXISTS people ( name TEXT, age INTEGER )').run();
+db.db.prepare('CREATE TABLE IF NOT EXISTS test ( name TEXT, age INTEGER )').run();
+db.insert('people', ['name', 'age'], ['Baby', 1]);
+db.insert('people', ['name', 'age'], ['Mia', 25]);
+db.insert('cats', ['name', 'age'], ['Xavi', 8]);
+db.insert('cats', ['name', 'age'], ['Sissy', 1]);
+db.insert('test', ['name', 'age'], ['test', 1]);
+console.log(db.tables());
+db.deleteTable('test');
+console.log(db.tables());
