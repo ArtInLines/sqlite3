dee6e142cfd2ec852f7a2a908252289fadac1c8d
ArtInLines <linesinart@gmail.com>
2021-09-04 23:25:40 +0200

    Updated & tested bind method

--- myTest.js
+++ myTest.js
@@ -1,10 +1,10 @@
 const SQLiteDB = require('./lib/SQLiteDB');
 
-let db = new SQLiteDB('temp.db', { verbose: console.log });
+let db = new SQLiteDB(':memory:', { verbose: null, bindToStr: true });
 
-db.db.prepare('CREATE TABLE IF NOT EXISTS cats ( name TEXT, age INTEGER, color TEXT )').run();
-db.db.prepare('CREATE TABLE IF NOT EXISTS people ( name TEXT, age INTEGER )').run();
-db.db.prepare('CREATE TABLE IF NOT EXISTS test ( name TEXT, age INTEGER )').run();
+db.prepare('CREATE TABLE IF NOT EXISTS cats ( name TEXT, age INTEGER, color TEXT )').run();
+db.prepare('CREATE TABLE IF NOT EXISTS people ( name TEXT, age INTEGER )').run();
+db.prepare('CREATE TABLE IF NOT EXISTS test ( name TEXT, age INTEGER )').run();
 db.insert('people', ['name', 'age'], ['Baby', 1]);
 db.insert('people', ['name', 'age'], ['Mia', 25]);
 db.insert('cats', ['name', 'age'], ['Xavi', 8]);
@@ -13,3 +13,8 @@ db.insert('test', ['name', 'age'], ['test', 1]);
 console.log(db.tables());
 db.deleteTable('test');
 console.log(db.tables());
+
+let query1 = db.bindToQuery('INSERT INTO ? (?,?)', null, 'cats', ['name', 'age']);
+let query2 = db.bindToQuery('VALUES(@name,@age), (?,?)', { quoteStr: true }, 'Kitty', 2, { name: 'cat', age: 9 });
+db.exec(query1 + ' ' + query2);
+console.log(db.bindToQuery('SELECT ?, ? FROM ?', { bindToStr: false }, 'name', 'age', 'cats').all());
