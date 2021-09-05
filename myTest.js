2119c993fc4f4c6b8060ea7744eee0a9fcb8829e
ArtInLines <linesinart@gmail.com>
2021-09-06 01:54:30 +0200

    Added tested replace method

--- myTest.js
+++ myTest.js
@@ -1,6 +1,6 @@
 const SQLiteDB = require('./lib/SQLiteDB');
 
-let db = new SQLiteDB(':memory:', { verbose: console.log, bindToStr: true });
+let db = new SQLiteDB(':memory:', { /* verbose: console.log, */ bindToStr: true });
 
 db.createTable({
 	name: 'cats',
@@ -10,18 +10,15 @@ db.createTable({
 		{ name: 'age', type: 'REAL' },
 		{ name: 'color', type: 'TEXT', default: 'grey' },
 	],
-	withoutRowID: true,
+	withoutRowID: false,
 	onUpdateDefaultAction: 'NO ACTION',
 	onDeleteDefaultAction: 'CASCADE',
 });
-let schema = db.schema('cats', true);
-db.deleteTable('cats');
-db.createTable({ ...schema, ...{ ifNotExists: true } });
 
 db.createTable({
 	name: 'people',
 	cols: [
-		{ name: 'person_id', type: 'REAL', primaryKey: true },
+		{ name: 'person_id', type: 'INTEGER', autoincrement: true, primaryKey: true },
 		{ name: 'name', type: 'TEXT', notNull: true, unique: true },
 		{ name: 'age', type: 'INTEGER', default: 18, notNull: true },
 		{ name: 'profile_pic', type: 'BLOB', unique: true },
@@ -30,9 +27,6 @@ db.createTable({
 		check: ['age >= 18', 'age <= 99'],
 	},
 });
-schema = db.schema('people', true);
-db.deleteTable('people');
-db.createTable({ ...schema, ...{ ifNotExists: true } });
 
 db.createTable({
 	name: 'people_cats_group',
@@ -52,31 +46,27 @@ db.createTable({
 	},
 	onDeleteDefaultAction: 'CASCADE',
 });
-schema = db.schema('people_cats_group', true);
-db.deleteTable('people_cats_group');
-db.createTable({ ...schema, ...{ ifNotExists: true } });
 
-console.log(db.tableNames());
+db.insert('cats', ['name', 'color', 'age'], ['Sissy', 'grey', 1], ['Kitty', 'greyish', null]);
+db.insert('cats', ['name', 'age', 'color'], 'Xavi', 8, 'orange');
+db.insert(
+	'people',
+	['name', 'age'],
+	[
+		['Firefly', 23],
+		['John Doe', 33],
+	]
+);
+db.insert('people', 'name', 'Art');
+// db.insert('people_cats_group', null, ['SELECT person_id FROM people WHERE name=Firefly', 0]);
 
-// db.insert('cats', ['name', 'color', 'age'], ['Sissy', 'grey', 1], ['Kitty', 'greyish', null]);
-// db.insert('cats', null, 'Xavi', 8, 'orange');
-// console.log(db.prepare('SELECT * FROM cats').all());
-// db.delete({ table: 'cats', filterCondition: "color LIKE '%grey%'" });
-// console.log(db.prepare('SELECT * FROM cats').all());
+console.log(db.prepare('SELECT * FROM cats').all());
+console.log(db.prepare('SELECT * FROM people').all());
+console.log(db.prepare('SELECT * FROM people_cats_group').all());
 
-// db.prepare('CREATE TABLE IF NOT EXISTS cats ( name TEXT, age INTEGER, color TEXT )').run();
-// db.prepare('CREATE TABLE IF NOT EXISTS people ( name TEXT, age INTEGER )').run();
-// db.prepare('CREATE TABLE IF NOT EXISTS test ( name TEXT, age INTEGER )').run();
-// db.insert('people', ['name', 'age'], ['Baby', 1]);
-// db.insert('people', ['name', 'age'], ['Mia', 25]);
-// db.insert('cats', ['name', 'age'], ['Xavi', 8]);
-// db.insert('cats', ['name', 'age'], ['Sissy', 1]);
-// db.insert('test', ['name', 'age'], ['test', 1]);
-// console.log(db.tables());
-// db.deleteTable('test');
-// console.log(db.tables());
+db.replace('people', ['name', 'age'], 'Jane Doe', 29);
+db.replace('people', ['person_id', 'name', 'age'], 2, 'John Doe', 34);
 
-// let query1 = db.bindToQuery('INSERT INTO ? (?,?)', null, 'cats', ['name', 'age']);
-// let query2 = db.bindToQuery('VALUES(@name,@age), (?,?)', { quoteStr: true }, 'Kitty', 2, { name: 'cat', age: 9 });
-// db.exec(query1 + ' ' + query2);
-// console.log(db.bindToQuery('SELECT ?, ? FROM ?', { bindToStr: false }, 'name', 'age', 'cats').all());
+console.log(db.prepare('SELECT * FROM cats').all());
+console.log(db.prepare('SELECT * FROM people').all());
+console.log(db.prepare('SELECT * FROM people_cats_group').all());
